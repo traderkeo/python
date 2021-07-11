675fe7c103ab201f75912c56b3a8e9cf5472f8d3
traderkeo <57576054+traderkeo@users.noreply.github.com>
2021-07-10 22:57:00 -0700

    Update main.py

--- finviz/api/sqliteDF/main.py
+++ finviz/api/sqliteDF/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 
-engine = create_engine('sqlite://', echo=False)
+engine = create_engine('sqlite:///vizfin.db', echo=False)
 
 
 class sql:
@@ -12,23 +12,26 @@ class sql:
     def __init__(self,table):
         self._table = table;
 
-    def create(self,df):
+    def create(self,df,desc):
         
+        dir = pd.DataFrame()
+        dir['table'] = [self._table]
+        dir['desc'] =  [desc]
+        dir.to_sql('directory',con=engine,if_exists='append',index_label="id")
         df.to_sql(self._table, con=engine, if_exists='fail',index_label='date')
         
         return engine.execute(f"SELECT * FROM {self._table}").fetchall()
     
     def empty(self):
-        engine.execute(f"DELETE * FROM {self._table}")
+        engine.execute(f"DELETE FROM {self._table}")
 
     def delete(self):
         engine.execute(f"DROP TABLE {self._table}")
     
     def update(self, data):
-        with engine.begin() as connection:
-            df2 = data
-            df2.to_sql(self._engine, con=engine, if_exists='append')
-            return engine.execute(f"SELECT * FROM {target}").fetchall()
+        df2 = data
+        df2.to_sql(self._engine, con=engine, if_exists='append')
+        return engine.execute(f"SELECT * FROM {target}").fetchall()
     
     def get(self, method):
         if method == 'sql':
